0a81ad80d592343a4c4272b95b7d7d61c225d380
Suvro Banerjee <suvrobaner@gmail.com>
2022-11-14 07:59:14 +0530

    valid parantheses

---
+++ algorithms/google/valid_parantheses.py
@@ -0,0 +1,30 @@
+class Solution:
+    # Time : O(n) | Space : O(n)
+    # where n is the length of the string
+    def isValid(self, s: str) -> bool:
+        char_map = {'(': ')', '{': '}', '[': ']'}
+        char_stack = []
+        for i in range(len(s)):
+            chr = s[i]
+            if chr in char_map:
+                char_stack.append(chr)
+            else:
+                if not char_stack:
+                    return False
+                prev_char = char_stack.pop()
+                if char_map[prev_char] != chr:
+                    return False
+        if char_stack:
+            return False
+        
+        return True
+
+s = "((){[()]})"
+s = "((("
+s = ")))"
+s = ")()"
+s = "(]"
+s = "()"
+s = "()[]{}"
+sol = Solution()
+print(sol.isValid(s))
